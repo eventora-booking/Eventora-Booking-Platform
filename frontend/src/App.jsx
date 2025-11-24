e18d0ea2fa4737976b87c49a57e9f90ad1a62e56
Eventora <eventora15@gmail.com>
2025-11-24 11:19:37 +0530

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-// Main App component - LandingPage handles all routing internally
-import LandingPage from './pages/LandingPage';
-
-function App() {
-  return <LandingPage />;
-}
-
-export default App;
